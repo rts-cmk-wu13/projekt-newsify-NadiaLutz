d529e42636b473d202b732e93c3162a63e305ea0
Nadia Lutz <na.lutz@outlook.com>
2025-05-08 10:45:59 +0200

    made the intro page and updated login

---
+++ src/intro.js
@@ -0,0 +1,32 @@
+export default function renderIntro(app, onComplete) {
+    app.innerHTML = "";
+    const container = document.createElement("div");
+    container.className = "intro-container";
+  
+    const logo = document.createElement("img");
+    logo.src = "./src/img/newsify_logo.png";
+    logo.className = "intro-logo";
+  
+    const text = document.createElement("div");
+    text.innerText = "Newsify";
+    text.className = "intro-text";
+  
+    container.appendChild(logo);
+    container.appendChild(text);
+    app.appendChild(container);
+  
+    setTimeout(() => {
+      logo.style.transform = "scale(2)";
+      logo.style.opacity = "1";
+    }, 100);
+  
+    setTimeout(() => {
+      text.style.opacity = "1";
+    }, 1200);
+  
+    setTimeout(() => {
+      container.style.opacity = "0";
+      setTimeout(() => onComplete(), 1000);
+    }, 2500);
+  }
+  
